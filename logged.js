4a958491fece43a0507e6e589ab22bb44936fd60
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-06 02:55:56 +0530

    Update logged.js

--- logged.js
+++ logged.js
@@ -7,8 +7,8 @@ function signOut() {
 function onSignIn(googleUser) {
 var profile = googleUser.getBasicProfile();
 if (window.location.href === "https://sahyog.netlify.com/signin.html#" || window.location.href === "https://sahyog.netlify.com/signin.html" ) {
-  window.location = "https://sahyog.netlify.com/";
-    location.reload();
+      location.reload();
+    window.location = "https://sahyog.netlify.com/";
 }
 document.querySelector('.signinbtn').setAttribute('style','display:none;');
 document.querySelector('.pcontent').setAttribute('style','display:block;');
