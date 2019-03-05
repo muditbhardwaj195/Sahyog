278bc6bf0f9bdba5ff22730f47d5ccaedcb3d922
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-05 21:11:44 +0530

    Add files via upload

--- logged.js
+++ logged.js
@@ -4,34 +4,6 @@ function signOut() {
       console.log('User signed out.');
     });
 }
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-/*
-var chk = getCookie("check");
-console.log('3.0');
-console.log(getCookie("check"));
-console.log(document.cookie);
-console.log(getCookie("username"));
-if (chk === "true") {
-document.querySelector('.signinbtn').setAttribute('style','display:none;');
-document.querySelector('.pcontent').setAttribute('style','display:block;');
-document.querySelector('.pcontent').innerHTML = ;
-} else {
-
-}*/
 function onSignIn(googleUser) {
 var profile = googleUser.getBasicProfile();
 console.log(profile.getName());
@@ -39,13 +11,3 @@ document.querySelector('.signinbtn').setAttribute('style','display:none;');
 document.querySelector('.pcontent').setAttribute('style','display:block;');
 document.querySelector('.pcontent').innerHTML = profile.getName();
 }
-/*
-var auth2 = gapi.auth2.init();
-if (auth2.isSignedIn.get()) {
-  console.log("Hi");
-    var profile = auth2.currentUser.get().getBasicProfile();
-    document.querySelector('.signinbtn').setAttribute('style','display:none;');
-    document.querySelector('.pcontent').setAttribute('style','display:block;');
-    document.querySelector('.pcontent').innerHTML = profile.getName();
-}  
-*/
