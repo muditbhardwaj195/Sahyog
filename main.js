fdefa8f52e3621fec2da3923c3a7fb527e852eef
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-05 19:39:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ document.querySelector('.nbtn').onclick = function() {
     document.querySelector('.dbtn').setAttribute('style','display:none;')
     document.querySelector('.abc').classList.remove('hideme');
 }
+document.querySelector('.dbtn').onclick = function() {
+    document.querySelector('.nbtn').setAttribute('style','display:none;')
+    document.querySelector('.dbtn').setAttribute('style','display:none;')
+    document.querySelector('.def').classList.remove('hideme');
+    document.querySelector('.dli').classList.remove('hideme');
+}
