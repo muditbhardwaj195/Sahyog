ca7962e4f67cd35a29d7f2a18d326bceac517d0b
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-05 20:33:35 +0530

    Add files via upload

--- logged.js
+++ logged.js
@@ -22,7 +22,8 @@ function getCookie(cname) {
 }
 var chk = getCookie("check");
 console.log('3.0');
-console.log(document.cookie());
+console.log(getCookie("check"));
+console.log(document.cookie("email"));
 console.log(getCookie("username"));
 if (chk === "true") {
 document.querySelector('.signinbtn').setAttribute('style','display:none;');
