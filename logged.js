dc86f64fbdaa5f0458dba09d0712d77b1bc779b4
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-05 21:02:49 +0530

    Add files via upload

--- logged.js
+++ logged.js
@@ -35,7 +35,6 @@ document.querySelector('.pcontent').innerHTML = ;
 var auth2 = gapi.auth2.init({
     client_id: '386445670275-fdce19rfmq07ll61o5n8i415noulm2f2.apps.googleusercontent.com',
     fetch_basic_profile: true,
-    scope: 'profile'
 });
 if (auth2.isSignedIn.get()) {
   
