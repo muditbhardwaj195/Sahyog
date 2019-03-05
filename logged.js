0750c6c1a9c9155513e7ddde541cf3ca18465fa1
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-05 20:13:40 +0530

    Add files via upload

---
+++ logged.js
@@ -0,0 +1,29 @@
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+var chk = getCookie("check");
+if (chk === "yes") {
+document.querySelector('.signinbtn').setAttribute('style','display:none;');
+document.querySelector('.pcontent').setAttribute('style','display:block;');
+document.querySelector('.pcontent').innerHTML = getCookie("name");
+} else {
+
+}
