273bf94e57d3db3dd44bc651d4b4b59c7a276d57
Nishant Mittal <36989112+nishantwrp@users.noreply.github.com>
2019-03-05 20:22:57 +0530

    Add files via upload

--- logged.js
+++ logged.js
@@ -1,6 +1,7 @@
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+        document.cookie="check=false"
       console.log('User signed out.');
     });
 }
